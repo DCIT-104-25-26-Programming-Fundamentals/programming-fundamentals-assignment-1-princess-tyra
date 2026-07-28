f31d42d470c495aee34c4408bb678652de495d2c
princess-tyra <fayambire001@st.ug.gh.edu>
2026-07-28 16:57:38 +0000

    tyra did

--- assignment_05_fibonacci_sequence.py
+++ assignment_05_fibonacci_sequence.py
@@ -49,3 +49,41 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def print_fibonacci sequence():
+n = int(input("How many terms? "))
+if n<=0:
+    print("Error: please enter a positive integer.")
+else:
+    a=0
+    b=1
+    print("Fibonacci sequence:", end="")
+    for i in range(n):
+        print(a, end="")
+        next_num = a + b
+        a = b
+        b = next_num
+        print()
+        def check_fibonacci_number():
+            target = int(input("Enter a number to check:" )
+        a , b, = 0,1
+        while a < target:
+        a ,b,= b, a+b
+        if a == target:
+        print(f"{target} is a Fibonacci number,")
+        else:
+        print(f"{target} is NOT a Fibonacci number. ")
+        print("--partA--")
+        print_fibonacci_sequence
+        print("\n--part B--")
+        check_fibonacci_number()
+
+        
+
+
+
+
+
+
+
+        
+              
